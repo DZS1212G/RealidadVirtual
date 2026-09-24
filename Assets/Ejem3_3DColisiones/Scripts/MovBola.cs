f8673f06e1d9e08b6cbd2cf3776fb9d0495b9be6
Diego Zapatero <diegozapatero1212@gmail.com>
2026-09-24 10:55:12 +0200

    Fin 23 sep 1clase

---
+++ Assets/Ejem3_3DColisiones/Scripts/MovBola.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MocBola : MonoBehaviour
+{
+    public int velocidad;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float movimientoHorizontal = Input.GetAxis("Horizontal");
+       float movimientoVertical = Input.GetAxis("Vertical");
+        this.transform.Translate(new Vector3(movimientoHorizontal, 0, movimientoVertical) * Time.deltaTime * velocidad,Space.World);
+    }
+}
